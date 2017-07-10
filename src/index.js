2d93b803bb692796e78ef49d8d8dda93f8fa4341
Zacharias Enochsson <zache@axis.com>
2017-07-11 01:04:10 +0200

    track movement to make FLIP work

--- src/index.js
+++ src/index.js
@@ -1,92 +1,134 @@
-function handler(prop, fn) {
-    return function (view) {
-        const orig = view.data[prop]
-        view.data[prop] = function (el, remove) {
-            orig && orig(el)
-            fn(el, remove)
-        }
-        return view
+
+const DEFAULTS = {
+  name: '',
+  time: 0,
+  delay: 0,
+  ready: 0,
+  easing: '',
+  last: true,
+}
+
+function setTransition (props, el) {
+  el.style.transition = `all ${props.easing} ${props.time}ms`
+}
+
+function txmethod (name, f) {
+  return function (props) {
+    props = Object.assign({}, DEFAULTS, props)
+    const handler = (...args) => f(props, ...args)
+    return function (vnode) {
+      const origHandler = vnode.data[name] || (_ => {})
+      vnode.data[name] = (...args) => {
+        origHandler(...args)
+        handler(...args)
+      }
+      return vnode
     }
+  }
 }
 
-function enter(fn, time, delay) {
-    if (time) return enter(_ => ({name: fn, time, delay}))
-    if (typeof fn !== 'function') return enter(_=>fn)
-    return handler('oncreate', function (el) {
-        const {name, time, delay} = fn()
-        const enterClass = name + '-enter'
-        const runClass = name + '-run'
-        el.classList.add(enterClass)
-        setTimeout(_ => {
-            el.classList.add(runClass)
-            el.classList.remove(enterClass)
-            setTimeout(_ => {
-                el.classList.remove(runClass)
-            }, time)
-        }, delay || 0)
-    })
-}
-
-function leave(fn, time, delay) {
-    if (time) return leave(_ => ({name: fn, time, delay}))
-    if (typeof fn !== 'function') return leave(_=>fn)
-    return handler('onremove', function (el, remove) {
-        const {name, time, delay} = fn()
-        const leaveClass = name + '-leave'
-        const runClass = name + '-run'
-        setTimeout(_ => {
-            el.classList.add(runClass)
-            el.classList.add(leaveClass)
-            setTimeout(function () {
-                el.classList.remove(runClass)
-                remove()
-            }, time)
-        }, delay || 0)
-    })
-}
-
-function change(fn) {
-    if (typeof fn !== 'function') return change(_=>fn)
-    return function (view) {
-        const name = fn()
-        view.data.class += ' ' + name + '-run'
-        return view
+function handler(prop, f) {
+  return function (vnode) {
+    const origHandler = vnode.data[prop] || (_ => {})
+    vnode.data[prop] = (...args) => {
+      origHandler(...args)
+      f(...args)
     }
+    return vnode
+  }
+} 
+
+function trackMoves (el) {
+    const prevX = el.getAttribute('data-t-x')
+    const prevY = el.getAttribute('data-t-y')
+    const {left: newX, top: newY} = el.getBoundingClientRect()
+    el.setAttribute('data-t-x', newX)
+    el.setAttribute('data-t-y', newY)
+    if (!prevX) return [null, null]
+    return [prevX - newX, prevY - newY]
 }
 
-function both (name, time, delay) {
-    const a = enter(name, time, delay)
-    const b = leave(name, time, delay)
-    return v => a(b(v))
+const _leaveOnRemove = txmethod('onremove', (props, el, remove) => {
+    const [dx, dy] = trackMoves(el)
+    el.style.transition = ''
+    el.style.position = 'relative'
+    el.style.top = dy + 'px'
+    el.style.left = dx + 'px'
+    setTimeout(_ => {
+      el.classList.add(`${props.name}-leave`)
+      setTransition(props, el)
+      setTimeout(_ => {
+        if (props.last) remove()
+      }, props.time)
+    }, props.delay)
+})
+
+const _leaveOnCreate = txmethod('oncreate', (props, el) => setTimeout(_ => trackMoves(el), props.ready))
+
+const leave = props => combine(
+  _leaveOnCreate(props),
+  _leaveOnRemove(props)
+)
+
+const enter = txmethod('oncreate', (props, el) => {
+  const clsEnter = `${props.name}-enter`
+  el.classList.add(clsEnter)
+  setTimeout(_ => {
+    setTransition(props, el)
+    el.classList.remove(clsEnter)
+    setTimeout(_ => {
+      el.style.transition = ''
+    }, props.time)
+  }, props.delay)
+})
+
+const change = txmethod('oncreate', (props, el) => setTransition(props, el))
+
+const _moveOnUpdate = txmethod('onupdate', (props, el) => {
+  const [dx, dy] = trackMoves(el)
+  el.style.transform = `translate(${dx}px, ${dy}px)`
+  setTimeout(_ => {
+    setTransition(props, el)
+    el.style.transition = `all ${props.easing} ${props.time}ms`
+    el.style.transform = 'translate(0,0)'
+    setTimeout(_ => {
+      el.style.transform = ''
+      el.style.transition = ''
+    }, props.time)
+  })        
+})
+
+const _moveOnCreate = txmethod('oncreate', (props, el) => setTimeout(_ => trackMoves(el), props.ready))
+
+const move = props => combine(
+  _moveOnCreate(props),
+  _moveOnUpdate(props)
+)
+
+function combine(...args) {
+  var a, b
+  const ts = [...args]
+  const l = ts.length
+  var a = ts[0]
+  if (l === 1) return a
+  var b
+  if (l > 2) b = combine(...ts.slice(1))
+  else b = ts[1]
+  return v => a(b(v))
 }
 
-function moves (fn, time) {
-    if (!fn)  return moves(_ => ({easing: '', time: 300}))
-    if (time) return moves(_ => ({easing: fn, time}))
-    if (typeof fn !== 'function') return moves(_ => fn)
-    return handler('onupdate', function (el) {
-        const {easing, time} = fn()
-        Array.from(el.childNodes).forEach(child => {
-            const {left: aX, top: aY} = child.getBoundingClientRect()
-            requestAnimationFrame(_ => {
-                const {left: bX, top: bY} = child.getBoundingClientRect()
-                child.style.transform = `translate(${aX-bX}px, ${aY-bY}px)`
-                requestAnimationFrame(_ => {
-                    child.style.transition = `all ${easing} ${time}ms`
-                    child.style.transform = ''
-                    setTimeout(_ => {
-                        child.style.transition = ''
-                    }, time)
-                })
-            })
-        })
-    })
+function group(f) {
+  return vnode => {
+    vnode.children.forEach(f)
+    return vnode
+  }
 }
 
 module.exports = {
     enter,
     leave,
-    both,
     change,
-    moves,
+    move,
+    combine,
+    group
 }
