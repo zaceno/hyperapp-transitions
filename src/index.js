853d8b7069ad918df07690218b8b3e6ac499e772
Zacharias Enochsson <zache@axis.com>
2017-10-11 02:32:17 +0200

    refactor to es5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const DEFAULTS = {
+var DEFAULTS = {
     name: '',
     time: 0,
     delay: 0,
@@ -7,17 +7,19 @@ const DEFAULTS = {
     last: true,
 }
 
+function noop () {}
+
 function removeElement (el) {
     if (!el.parentNode) return
     el.parentNode.removeChild(el)
 }
 
 function setTransition (props, el) {
-    el.style.transition = `all ${props.easing} ${props.time}ms`
+    el.style.transition = 'all ' + props.easing + ' ' + props.time + 'ms'
 }
 
 function props2Fn(props) {
-    return _ => {
+    return function () {
         var props2 = (typeof props === 'function') ? props() : props
         return Object.assign({}, DEFAULTS, props2)
     }
@@ -25,15 +27,15 @@ function props2Fn(props) {
 
 function txmethod (name, f) {
     return function (props) {
-        const propsFn = props2Fn(props)
-        const handler = el => f(propsFn(), el)
+        var propsFn = props2Fn(props)
+        var handler = function (el) { return f(propsFn(), el) }
         return function (vnode) {
             if (!vnode || !vnode.props) return
-            const origHandler = vnode.props[name] || (_ => {})
-            vnode.props[name] = (...args) => {
-                origHandler(...args)
-                handler(...args)
-                return () => {} //hack to make onremove let me remove elements myself
+            var origHandler = vnode.props[name] || (noop)
+            vnode.props[name] = function (el) {
+                origHandler(el)
+                handler(el)
+                return noop //hack to make onremove let me remove elements myself
             }
             return vnode
         }
@@ -41,97 +43,123 @@ function txmethod (name, f) {
 }
 
 function trackMoves (el) {
-    const prevX = +el.getAttribute('data-t-x')
-    const prevY = +el.getAttribute('data-t-y')
-    const {left: newX, top: newY} = el.getBoundingClientRect()
+    var prevX = +el.getAttribute('data-t-x')
+    var prevY = +el.getAttribute('data-t-y')
+    var rect = el.getBoundingClientRect()
+    var newX = rect.left
+    var newY = rect.top
     el.setAttribute('data-t-x', newX)
     el.setAttribute('data-t-y', newY)
     if (!prevX) return [null, null]
     return [prevX - newX, prevY - newY]
 }
 
-const _leaveOnRemove = txmethod('onremove', (props, el) => {
-    const cls = `${props.name}-leave`
+var _leaveOnRemove = txmethod('onremove', function (props, el) {
+    var cls = props.name + '-leave'
 
     //first we need to capture any transforms the
     //leave class will apply
     el.style.transition = ''
     el.style.transform = ''
     el.classList.add(cls)
-    const willTransform = getComputedStyle(el).getPropertyValue('transform')
-    const [sx, wx, wy, sy, tx, ty] = (willTransform === 'none')
+    var willTransform = getComputedStyle(el).getPropertyValue('transform')
+    willTransform = (willTransform === 'none')
         ? [1, 0, 0, 1, 0, 0]
-        : willTransform.match(/^matrix\(([^\)]*)\)$/)[1].split(', ').map(s => +s);
+        : willTransform.match(/^matrix\(([^\)]*)\)$/)[1].split(', ').map(function (s) { return +s })
+    var sx = willTransform[0],
+        wx = willTransform[1],
+        wy = willTransform[2],
+        sy = willTransform[3],
+        tx = willTransform[4],
+        ty = willTransform[5];
     el.classList.remove(cls)
 
     //and set the plain translation:
-    const [dx, dy] = trackMoves(el)
-    el.style.transform = `translate(${dx}px, ${dy}px)`
+    var trackDelta = trackMoves(el)
+    var dx = trackDelta[0]
+    var dy = trackDelta[1]
+    el.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)'
     
     //after the specified delay, apply the transition
-    setTimeout(_ => {
+    setTimeout(function () {
         el.classList.add(cls)
         //override the transition props
-        el.style.transform = `matrix(${sx}, ${wx}, ${wy}, ${sy}, ${tx + dx}, ${ty + dy})`
+        el.style.transform = 'matrix(' + [sx, wx, wy, sy, tx + dx, ty + dy].join(',') + ')'
         setTransition(props, el)
         //after the delay, if last, remove:
-        if (props.last) setTimeout(_ => removeElement(el), props.time)
+        if (props.last) setTimeout(function () {removeElement(el)}, props.time)
     }, props.delay)
 })
 
-const _leaveOnCreate = txmethod('oncreate', (props, el) => setTimeout(_ => trackMoves(el), props.ready))
+var _leaveOnCreate = txmethod('oncreate', function (props, el) {
+    return setTimeout(function () {
+        return trackMoves(el)
+    }, props.ready)
+})
 
-const leave = props => combine(
-    _leaveOnCreate(props),
-    _leaveOnRemove(props)
-)
+var leave = function (props) {
+    return combine(
+        _leaveOnCreate(props),
+        _leaveOnRemove(props)
+    )
+}
 
-const enter = txmethod('oncreate', (props, el) => {
-    const clsEnter = `${props.name}-enter`
+var enter = txmethod('oncreate', function (props, el) {
+    var clsEnter = props.name + '-enter'
     el.classList.add(clsEnter)
-    setTimeout(_ => {
+    setTimeout(function () {
         setTransition(props, el)
         el.classList.remove(clsEnter)
     }, props.delay)
 })
 
-const change = txmethod('oncreate', (props, el) => setTransition(props, el))
+var change = txmethod('oncreate', function (props, el) {
+    return setTransition(props, el)
+})
 
-const _moveOnUpdate = txmethod('onupdate', (props, el) => {
-    const [dx, dy] = trackMoves(el)
+var _moveOnUpdate = txmethod('onupdate', function (props, el) {
+    var move = trackMoves(el)
+    var dx = move[0]
+    var dy = move[1]
     el.style.transition = ''
-    el.style.transform = `translate(${dx}px, ${dy}px)`
-    setTimeout(_ => {
+    el.style.transform = 'translate(' + dx + ', ' + dy + 'px)'
+    setTimeout(function () {
         setTransition(props, el)
         el.style.transform = 'translate(0,0)'
-        setTimeout(_ => {
+        setTimeout(function () {
             el.style.transform = ''
             el.style.transition = ''
         }, props.time)
-    })        
+    }, 0)        
 })
 
-const _moveOnCreate = txmethod('oncreate', (props, el) => setTimeout(_ => trackMoves(el), props.ready))
+var _moveOnCreate = txmethod('oncreate', function (props, el) {
+    setTimeout(function () {
+        trackMoves(el)
+    }, props.ready)
+})
 
-const move = props => combine(
-    _moveOnCreate(props),
-    _moveOnUpdate(props)
-)
+var move = function (props) {
+    return combine(
+        _moveOnCreate(props),
+        _moveOnUpdate(props)
+    )
+} 
 
-function combine(...args) {
+function combine() {
     var a, b
-    const ts = [...args]
-    const l = ts.length
+    var ts = Array.prototype.slice.call(arguments)
+    var l = ts.length
     var a = ts[0]
     if (l === 1) return a
     var b
-    if (l > 2) b = combine(...ts.slice(1))
+    if (l > 2) b = combine.apply(null, ts.slice(1))
     else b = ts[1]
-    return v => a(b(v))
+    return function (v) { return a(b(v)) }
 }
 
 function group(f) {
-    return vnode => {
+    return function (vnode) {
         vnode.children.forEach(f)
         return vnode
     }
@@ -139,10 +167,10 @@ function group(f) {
 
 
 module.exports = {
-    change,
-    enter,
-    leave,
-    move,
-    group,
-    combine
+    change: change,
+    enter: enter,
+    leave: leave,
+    move: move,
+    group: group,
+    combine: combine,
 }
